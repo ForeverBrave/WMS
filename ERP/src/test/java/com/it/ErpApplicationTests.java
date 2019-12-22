b9ffc240ba0bdd6d5b47befa62d1f09e7d85081e
Brave <734856476@qq.com>
2019-12-22 20:13:22 +0800

    修复了一些BUG

---
+++ ERP/src/test/java/com/it/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
