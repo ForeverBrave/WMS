fdb78bce144fa3d92f84b1a1fd5b3adad6efb828
Brave <734856476@qq.com>
2019-12-23 18:28:35 +0800

    修复了一些BUG

--- ERP/src/main/java/com/it/ErpApplication.java
+++ ERP/src/main/java/com/it/ErpApplication.java
@@ -1,9 +1,11 @@
 package com.it;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.it.sys.mapper")
 public class ErpApplication {
 
 	public static void main(String[] args) {
